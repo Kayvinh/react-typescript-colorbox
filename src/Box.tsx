0ae89e60bf7393caa395e3d2edb9dac1a7bdfd47
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-05-01 15:21:47 -0700

    initializing

---
+++ src/Box.tsx
@@ -0,0 +1,49 @@
+import { BoxInterface } from "./BoxList";
+
+/** Colored box presentation
+ *
+ * Props:
+ * - id (unique)
+ * - width
+ * - height
+ * - backgroundColor
+ * - remove (function to call)
+ * 
+ * State:
+ * - none
+ *
+ * BoxList -> Box
+ */
+
+interface BoxWithRemoveInterface extends BoxInterface {
+  remove: Function;
+}
+
+
+function Box({ id, width = 5, height = 5, backgroundColor, remove }
+  :BoxWithRemoveInterface ) {
+
+  /** Remove a box. */
+  function handleRemove(): void{
+    remove(id);
+  }
+
+  return (
+    <div className="Box">
+      <div className="Box-box"
+        style={{
+          height: `${height}em`,
+          width: `${width}em`,
+          backgroundColor: backgroundColor
+        }}
+      />
+      <button
+        className="Box-removeBtn"
+        onClick={handleRemove}>
+        Remove The Box!
+      </button>
+    </div>
+  );
+}
+
+export default Box;
---
+++ src/BoxList.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
+
+export interface BoxInterface {
+  id: string,
+  width: number,
+  height: number,
+  backgroundColor: string,
+}
+
+/** Manage list of boxes
+ * 
+ * Props:
+ * -none
+ *
+ * State:
+ * - boxes: [ { id, width, height, backgroundColor }, ... ]
+ * 
+ * App -> BoxList -> { NewBoxForm, Box }
+ */
+
+function BoxList() {
+  const [boxes, setBoxes] = useState<BoxInterface[]>([]);
+
+  /** add box with given { id, width, height, backgroundColor } */
+  function add(newBox: BoxInterface): void {
+    setBoxes(boxes => [...boxes, newBox]);
+  }
+
+  /** remove box matching that id. */
+  function remove(id: string): void {
+    setBoxes(boxes => boxes.filter(box => box.id !== id));
+  }
+
+  return (
+    <div className="BoxList">
+      <NewBoxForm createBox={add} />
+      {boxes.map(({ id, width, height, backgroundColor }) => (
+        <Box
+          key={id}
+          id={id}
+          width={width}
+          height={height}
+          remove={remove}
+          backgroundColor={backgroundColor}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default BoxList;
